942242df65d7994baa3512e85835a66438d8ee68
ge-kim <gekim@hanpass.com>
2022-06-22 13:04:28 +0900

    feat: 문자열 덧셈 계산기

---
+++ src/main/java/racingcar/StringAddCalculator.java
@@ -0,0 +1,46 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final Pattern DEFAULT_DELIMITER = Pattern.compile("[,:]");
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("^//(.)\n(.*)$");
+
+    public static int splitAndSum(String input) {
+        if(input == null || input.trim().isEmpty()) {
+            return 0;
+        }
+
+        if(!input.startsWith("//")) {
+            return sum(input, DEFAULT_DELIMITER.pattern());
+        }
+
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (matcher.find()) {
+            String delimiter = matcher.group(1);
+            String expression = matcher.group(2);
+
+            return sum(expression, delimiter);
+        }
+
+        return 0;
+    }
+
+    private static int sum(String expression, String delimiter) {
+        return Arrays.stream(expression.split(delimiter))
+                .reduce(0, filteredDigit(), Integer::sum);
+
+    }
+
+    private static BiFunction<Integer, String, Integer> filteredDigit(){
+        return (accumulated, nextValue) -> {
+            if (!nextValue.matches("\\d") || Integer.parseInt(nextValue) < 0) {
+                throw new IllegalArgumentException("You must input String only with positive number");
+            }
+            return accumulated + Integer.parseInt(nextValue);
+        };
+    }
+}
---
+++ src/test/java/racingcar/ReduceTest.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public class ReduceTest {
+
+
+    @Test
+    void reduceTest(){
+        String input = "1,2,3,4";
+        Integer reduce = Arrays.stream(input.split(","))
+                .reduce(0, filteredDigit(), Integer::sum);
+
+        Assertions.assertThat(reduce).isEqualTo(10);
+    }
+
+    //  BiFunction<T, U, R>
+    //  <T> the type of the first argument to the function
+    //  <U> the type of the second argument to the function
+    //  <R> the type of the result of the function
+   private BiFunction<Integer, String, Integer> filteredDigit(){
+        return (accumulated, nextValue) -> {
+            if (!nextValue.matches("\\d") || Integer.parseInt(nextValue) < 0) {
+                throw new IllegalArgumentException("You must input String only with positive number");
+            }
+            return accumulated + Integer.parseInt(nextValue);
+        };
+    }
+
+}
